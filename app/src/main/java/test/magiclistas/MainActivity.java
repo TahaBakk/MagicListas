a48bd92a50d38c32890c0e603a532a7fd33b4423
TahaBakk <tahabakk2@gmail.com>
2016-11-20 19:51:35 +0100

    Modificat el intent

--- app/src/main/java/test/magiclistas/MainActivity.java
+++ app/src/main/java/test/magiclistas/MainActivity.java
@@ -21,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+asdasdasd
+
+
+
 
 }
